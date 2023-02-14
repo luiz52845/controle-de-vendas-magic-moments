8c5117048ebc07796272e9266c4e535b89a603f3
luiz <luizes_@hotmail.com>
2023-02-13 21:00:54 -0300

    teste

---
+++ Controle-de-vendas/src/main/java/io/github/luizfernandes/Controle/de/vendas/ControleDeVendasApplication.java
@@ -0,0 +1,15 @@
+package io.github.luizfernandes.Controle.de.vendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleDeVendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleDeVendasApplication.class, args);
+
+		//teste
+	}
+
+}
---
+++ Controle-de-vendas/src/test/java/io/github/luizfernandes/Controle/de/vendas/ControleDeVendasApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.luizfernandes.Controle.de.vendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleDeVendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
